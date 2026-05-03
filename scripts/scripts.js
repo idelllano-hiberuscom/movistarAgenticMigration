8f4b0e53af80f7f66ef862c4bce8c9d0500b9c89
ivaanblanco <ivan.delllano.blanco2004@gmail.com>
2026-05-03 18:23:57 +0200

    integration experiments

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -3,6 +3,7 @@ import {
   decorateIcons,
   decorateSections,
   decorateTemplateAndTheme,
+  getMetadata,       
   loadCSS,
   loadFooter,
   loadHeader,
@@ -11,6 +12,27 @@ import {
   waitForFirstImage,
 } from './aem.js';
 
+function getAllMetadata(scope) {
+  return [...document.head.querySelectorAll(`meta[name^="${scope}-"],meta[property^="${scope}:"]`)]
+    .reduce((res, meta) => {
+      const key = meta.name ? meta.name.replace(`${scope}-`, '') : meta.getAttribute('property').replace(`${scope}:`, '');
+      res[key] = meta.content;
+      return res;
+    }, {});
+}
+
+const pluginContext = {
+  getAllMetadata,
+  getMetadata,
+  decorateBlock: () => {},
+  decorateButtons: () => {},
+  decorateIcons,
+  decorateSections,
+  decorateBlocks,
+  loadBlock: () => Promise.resolve(),
+  loadCSS,
+  sampleRUM,
+};
 
 function decorateMain(main) {
   decorateSections(main);
@@ -25,10 +47,16 @@ async function loadEager(doc) {
   document.documentElement.lang = 'es';
   decorateTemplateAndTheme();
 
+  if (getMetadata('experiment')
+    || Object.keys(getAllMetadata('campaign')).length
+    || Object.keys(getAllMetadata('audience')).length) {
+    const { loadEager: runEager } = await import('../plugins/experimentation/src/index.js');
+    await runEager(document, {}, pluginContext);
+  }
+
   const main = doc.querySelector('main');
   if (main) {
     decorateMain(main);
-
     const firstSection = main.querySelector('.section');
     if (firstSection) {
       await waitForFirstImage(firstSection);
@@ -41,20 +69,25 @@ async function loadLazy(doc) {
   if (main) {
     await loadSections(main);
   }
-
   const header = doc.querySelector('header');
   if (header) {
     await loadHeader(header);
   }
-
   const footer = doc.querySelector('footer');
   if (footer) {
     await loadFooter(footer);
   }
-
   loadCSS('/styles/fonts.css');
   loadCSS('/styles/lazy-styles.css');
   sampleRUM('lazy');
+
+  // ← AÑADIR ESTO AL FINAL DE loadLazy
+  if (getMetadata('experiment')
+    || Object.keys(getAllMetadata('campaign')).length
+    || Object.keys(getAllMetadata('audience')).length) {
+    const { loadLazy: runLazy } = await import('../plugins/experimentation/src/index.js');
+    await runLazy(document, {}, pluginContext);
+  }
 }
 
 function loadDelayed() {
